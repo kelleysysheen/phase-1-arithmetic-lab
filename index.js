35fc68edd6a5f0ff666bfe59c5e97e5af60c7716
Kelley Sheen <kelleysheen@gmail.com>
2023-09-21 21:50:50 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
-// Write your code here
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+
+let decimal = Math.random();
+let randomNumber = (decimal * 10 * Math.random());
+let random = Math.round(randomNumber);
+
+console.log('THIS IS THE NUMBER:' + random);
+
+
+Math.floor(Math.random() * 10);
+
+
+let num3= 16;
+let num4= 4;
+let mod = num3/num4;
+
+let max = 20
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
